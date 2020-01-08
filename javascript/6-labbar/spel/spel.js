a7e99326410dcbd75b8bd7a24a6206582c504e76
Dominika Dlugowolska <dominika.dlugowolsk@learnet.se>
2020-01-08 13:31:55 +0100

    Hitta-fel, spel

---
+++ javascript/6-labbar/spel/spel.js
@@ -0,0 +1,36 @@
+/* Sport, fritid, blandat
+
+Efter en imponerande lång rad SM-guld vinner brottaren Pelle "Swing" Svensson sitt första VM-guld.akti
+1970
+
+Peter "Foppa" Forsberg gör sin första säsong i Colorado Avalanche.
+1995
+
+"Lev skönare!" är Johan Holmsäters motto när han grundar Friskis och Svettis
+1978
+
+Sju Mancherster United-spelare dör i en flygolycka.
+1958
+
+Prylar, nyheter, uppfinningar
+
+Erik Wallenberg uppfinner den pyramidformade tetraförpackningen.
+1944
+
+Christer Fuglesang tar sin första rymdpromenad.
+2006
+
+Kända personer och händelser
+
+På Gärdet i Stockholm ockuperas västtyska ambassaden och gisslandramat som följer kommer att kräva fyra liv.
+1975
+
+Kinas siste kejsare dör.
+1967
+
+Den fjärde oktober demonstrerar 75 000 mot införandet av löntagarfonder.
+1983
+
+Göran Persson gifter sig med Anitra Steen.
+2003
+*/
